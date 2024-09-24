c35b8fee1837590f4958125686c2cb69609cea89
Mehedi Hasan Parash <mehedihasan@Mehedis-Laptop.local>
2024-09-25 00:54:21 +0600

    ExoNav Ai Server fisrt commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ExoNav-Server
---
+++ index.js
@@ -0,0 +1,164 @@
+import express from "express";
+import cors from "cors";
+import ImageKit from "imagekit";
+import mongoose from "mongoose";
+import UserChats from "./models/userchats.js";
+import Chat from "./models/chat.js";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongodb");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+  publicKey: process.env.IMAGE_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_PRIVATE_KEY,
+});
+
+app.get("/api/upload", (req, res) => {
+  const result = imagekit.getAuthenticationParameters();
+  res.send(result);
+});
+
+app.post("/api/chats", ClerkExpressRequireAuth(), async (req, res) => {
+  const { userId } = req.auth;
+  const { text } = req.body;
+  try {
+    // CREATE A NEW CHAT
+    const newChat = new Chat({
+      userId: userId,
+      history: [{ role: "user", parts: [{ text }] }],
+    });
+
+    const savedChat = await newChat.save();
+
+    // CHECK IF THE USER CHAT EXISTS
+    const userChats = await UserChats.find({
+      userId: userId,
+    });
+    // IF DOESNOT EXISTS CREATE A NEW ONE AND ADD IN THE CHATS ARRAY
+    if (!userChats.length) {
+      const newUserChats = new UserChats({
+        userId: userId,
+        chats: [
+          {
+            _id: savedChat._id,
+            title: text.substring(0, 40),
+          },
+        ],
+      });
+
+      await newUserChats.save();
+    } else {
+      // IF EXISTS PUSH IT ON EXISTING ARRAY
+      await UserChats.updateOne(
+        { userId: userId },
+        {
+          $push: {
+            chats: {
+              _id: savedChat._id,
+              title: text.substring(0, 40),
+            },
+          },
+        }
+      );
+
+      res.status(201).send(newChat._id);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error creating chat!");
+  }
+});
+
+app.get("/api/userchats", ClerkExpressRequireAuth(), async (req, res) => {
+  const { userId } = req.auth;
+
+  try {
+    const userChats = await UserChats.find({ userId });
+    res.status(200).send(userChats[0].chats);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching userchats!");
+  }
+});
+
+app.get("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+  const { userId } = req.auth;
+
+  try {
+    const chat = await Chat.findOne({ _id: req.params.id, userId });
+    res.status(200).send(chat);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching chat!");
+  }
+});
+
+app.put("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+  const { userId } = req.auth;
+
+  const { question, answer, img } = req.body;
+
+  const newItems = [];
+
+  // Push user's message if available
+  if (question) {
+    const userMessage = { role: "user", parts: [{ text: question }] };
+    if (img) {
+      userMessage.img = img;
+    }
+    newItems.push(userMessage);
+  }
+
+  // Push model's message if available
+  if (answer) {
+    newItems.push({ role: "model", parts: [{ text: answer }] });
+  }
+
+
+  try {
+    const updatedChat = await Chat.updateOne(
+      { _id: req.params.id, userId },
+      {
+        $push: {
+          history: {
+            $each: newItems,
+          },
+        },
+      }
+    );
+    res.status(200).send(updatedChat);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error adding conversation!");
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(401).send("Unauthenticated!");
+});
+
+app.listen(port, () => {
+  connect();
+  console.log("Server is running on 3000");
+});
